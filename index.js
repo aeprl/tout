015633c5acdb1414a1dd69e0be6d3892abd90388
aeprl <aeprl@outlook.com>
2020-07-30 13:33:25 -0400

    nvm ig

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-if(!message.content.startsWith(prefix))return;
 
 client.once('ready', () => {
 	client.user.setActivity('a', { type: 'LISTENING' });

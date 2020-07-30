9ee15b158099355c8885632c98eb24696ebd1636
aeprl <aeprl@outlook.com>
2020-07-30 15:41:03 -0400

    testing commands

---
+++ commands/beep the boop.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+	name: 'beep the boop',
+	description: 'boop.',
+	execute(message, args) {
+		message.reply('beep beeped');
+	},
+};
---
+++ commands/beep the bop.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+	name: 'beep the bop',
+	description: 'beep.',
+	execute(message, args) {
+		message.reply('beep bopped');
+	},
+};

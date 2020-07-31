50a13f58337d47043eeea2202e9507b2d7a9b79b
aeprl <aeprl@outlook.com>
2020-07-31 12:01:48 -0400

    fixed it uh oh

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -5,7 +5,7 @@ const commandNumber = fs.readdirSync('./commands').length;
 module.exports = {
 	name: 'botinfo',
 	description: 'shows the info about the bot',
-	alias: 'bi'
+	alias: 'bi',
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
 		const toutEmbed = new Discord.MessageEmbed()

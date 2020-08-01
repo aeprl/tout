e6b34dd20f756436f4bb56ec2b0bcb21bd3d9f7f
aeprl <aeprl@outlook.com>
2020-07-31 23:51:18 -0400

    added avatar command
    not the movie btw

---
+++ commands/avatar.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'avatar',
+	description: 'get your own or the mentioned persons avatar.',
+	execute(message, args) {
+		const avatarEmbed = new Discord.MessageEmbed;
+		if(!message.mentions.users.first()) {
+			avatarEmbed.setTitle('your avatar');
+			avatarEmbed.setThumbnail(message.author.displayAavatarURL());
+			avatarEmbed.setColor('acc6eb');
+			avatarEmbed.setFooter('by aeprl#0031', 'https://cdn.discordapp.com/attachments/472189537162887180/737423238375800837/pfp3.png');
+			return message.reply(`here you go ${avatarEmbed}`);
+		}
+		else{
+			const mentionedUser = message.mentions.users.first();
+			avatarEmbed.setTitle(`${mentionedUser.tag}'s profile picture`);
+			avatarEmbed.setThumbnail(mentionedUser.displayAavatarURL());
+			avatarEmbed.setColor('acc6eb');
+			avatarEmbed.setFooter('by aeprl#0031', 'https://cdn.discordapp.com/attachments/472189537162887180/737423238375800837/pfp3.png');
+			return message.reply(`here you go ${avatarEmbed}`);
+		}
+	},
+};

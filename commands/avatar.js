61e6c9a72b17dcd42e2d674324370ffb0af0a0c4
aeprl <aeprl@outlook.com>
2020-08-01 01:36:59 -0400

    nocredit

--- commands/avatar.js
+++ commands/avatar.js
@@ -13,14 +13,11 @@ module.exports = {
 		if(!message.mentions.users.first()) {
 			avatarEmbed.setThumbnail(message.author.displayAvatarURL());
 			avatarEmbed.setColor('acc6eb');
-			avatarEmbed.setFooter('by aeprl#0031', 'https://cdn.discordapp.com/avatars/162945458581274624/854975aa1a09abcc8e1354b3252f6457.png?size=256');
-			return message.channel.send(avatarEmbed);
 		}
 		else{
 			const mentionedUser = message.mentions.users.first();
 			avatarEmbed.setThumbnail(mentionedUser.displayAvatarURL());
 			avatarEmbed.setColor('acc6eb');
-			avatarEmbed.setFooter('by aeprl#0031', 'https://cdn.discordapp.com/avatars/162945458581274624/854975aa1a09abcc8e1354b3252f6457.png?size=256');
 			return message.channel.send(avatarEmbed);
 		}
 	},

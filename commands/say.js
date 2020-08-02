7c9cad4d8a3eb5c7b0927117fe54f57b4c53e286
aeprl <aeprl@outlook.com>
2020-08-02 08:43:20 -0400

    fix args

--- commands/say.js
+++ commands/say.js
@@ -6,6 +6,7 @@ module.exports = {
 	description: 'say things as the bot.',
 	execute(message, args) {
         
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`you can't use that without permissions`)
         const sayMessage = args.join('');
         message.delete().catch();

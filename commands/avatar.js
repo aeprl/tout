faf6a7104deb1b222f0d8ca3821ddb6386a03dfa
aeprl <aeprl@outlook.com>
2020-08-02 09:37:32 -0400

    from webp to png

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,18 +7,16 @@ module.exports = {
 	description: 'get your own or the mentioned persons avatar.',
 	execute(message, args, client) {
 
-		message.client.user.avatarURL({ format: 'png', dynamic: true, size: 1024 });
-
 		const avatarEmbed = new Discord.MessageEmbed();
 		if(!message.mentions.users.first()) {
-			avatarEmbed.setAuthor(message.author.username, message.author.displayAvatarURL());
+			avatarEmbed.setAuthor(message.author.username, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 500 }));
 			avatarEmbed.setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
 			avatarEmbed.setColor('acc6eb');
 			return message.channel.send(avatarEmbed);
 		}
 		else{
 			const mentionedUser = message.mentions.users.first();
-			avatarEmbed.setAuthor(message.author.username, message.author.displayAvatarURL());
+			avatarEmbed.setAuthor(message.author.username, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 500 }));
 			avatarEmbed.setImage(mentionedUser.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
 			avatarEmbed.setColor('acc6eb');
 			return message.channel.send(avatarEmbed);

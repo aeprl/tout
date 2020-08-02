9364e4d7d43719d60f356db4425c17ee5d2cffb6
aeprl <aeprl@outlook.com>
2020-08-02 09:02:57 -0400

    made avatar url in .png and bigger

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
-const prefix = require('../config.json')
 
 module.exports = {
 	name: 'say',
 	description: 'say things as the bot.',
-	execute(message) {
+	execute(message, args) {
         
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`you can't use that without permissions`)
         const sayMessage = args.join('');
         message.delete().catch();

2c6001609aa895e8702c992f20d8984f6f136f21
aeprl <aeprl@outlook.com>
2021-01-10 20:32:20 +0300

    oops

--- commands/pizza.js
+++ commands/pizza.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-	name: 'ping',
-	description: 'ping.',
+	name: 'pizza',
+	description: 'ay yo the pizza here.',
 	execute(message, args) {
 		message.channel.send('https://tenor.com/view/ay-yo-the-pizza-here-initial-d-monkey-moments-cord-monkey-gif-18954890');
 	},

987ebac207d9b0394370741c67b87054ea008c8f
aeprl <aeprl@outlook.com>
2021-01-10 20:31:24 +0300

    pizza

---
+++ commands/pizza.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+	name: 'ping',
+	description: 'ping.',
+	execute(message, args) {
+		message.channel.send('https://tenor.com/view/ay-yo-the-pizza-here-initial-d-monkey-moments-cord-monkey-gif-18954890');
+	},
+};

ddf61675ca549c148b6005bc3de6117561c3197d
aeprl <aeprl@outlook.com>
2021-01-10 21:53:56 +0300

    say

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const commandNumber = fs.readdirSync('./commands').length;
+
+module.exports = {
+	name: 'say',
+	description: 'says',
+	// eslint-disable-next-line no-unused-vars
+	execute(message, args) {
+        const sayM = args.join(" ");
+        message.delete().catch(err => console.log(err));
+        message.channel.send(sayM);
+	},
+
+};

73145702f28480fcf81ef41b95177d38063f142f
aeprl <aeprl@outlook.com>
2021-04-18 20:40:17 +0300

    serverinfo

---
+++ commands/serverinfo.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const{ name, memberCount, owner} = guild
+const icon = guild.iconURL()
+
+module.exports = {
+	name: 'serverinfo',
+	description: 'server info',
+	alias: 'bi',
+	// eslint-disable-next-line no-unused-vars
+	execute(message, args) {
+		const toutInfo = new Discord.MessageEmbed()
+			.setColor('acc6eb')
+			.setTitle(`${name} info`)
+			.setURL('https://www.youtube.com/watch?v=VD6__C2Ht6M')
+			.setFooter('by aeprl#0031')
+			.setThumbnail(icon)
+			.addFields(
+				{ name : `${guild.name}` },
+				{ name : 'member count', value : `${memberCount}`, inline: true },
+				{ name : 'owner tag', value : `${owner.user.tag}`, inline: true },
+			)
+			.setTimestamp();
+
+		message.channel.send(toutInfo);
+	},
+
+};

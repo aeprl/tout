4f45238eff897b325bb71d517ec8df8f66fa283c
aeprl <aeprl@outlook.com>
2021-04-18 20:47:06 +0300

    uhhh

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const{ name, memberCount, owner} = guild
+const{ name, memberCount, owner} = message.guild
 const icon = guild.iconURL()
 
 module.exports = {
